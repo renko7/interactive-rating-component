4c8490e5f7b6e04e1823dc850963e93b1d4d47d7
renko7 <66572823+renko7@users.noreply.github.com>
2022-03-31 19:57:27 -0400

    importing css colors to js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = getComputedStyle(document.body)
+const colorNeutralWhite = body.getPropertyValue('--clr-neutral-white')
+const colorLightGrey = body.getPropertyValue('--clr-neutral-light-grey')
+const colorMediumGrey = body.getPropertyValue('--clr-neutral-medium-grey')
+const colorVeryDarkBlue = body.getPropertyValue('--clr-neutral-very-dark-blue')
+
